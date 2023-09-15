3f3617bf5fa469285cf0f31774e6327f02c520d9
Bo Yang Yu <boyangyu93@gmail.com>
2023-09-14 21:18:36 -0400

    create helper function to display three of the same image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,17 +15,26 @@ const sampleDataForPhotoListItem = {
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
 
+const photosObject = new Array(3).fill(sampleDataForPhotoListItem
+  );
+
+const listOfPhoto = photosObject.map((photo, index) => {
+  return (
+    <PhotoListItem 
+      key={index} 
+      imageSource={photo.imageSource} 
+      username={photo.username}
+      profile={photo.profile}
+      city={photo.location.city}
+      country={photo.location.country}/>
+  )
+})
+
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem 
-      key={sampleDataForPhotoListItem.key} 
-      imageSource={sampleDataForPhotoListItem.imageSource} 
-      username={sampleDataForPhotoListItem.username}
-      profile={sampleDataForPhotoListItem.profile}
-      city={sampleDataForPhotoListItem.location.city}
-      country={sampleDataForPhotoListItem.location.country}/>
+      {listOfPhoto}
     </div>
   );
 };
